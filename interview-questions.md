4583e8af8819166b01c7bebf40dbbef5fed6896e
anthonyholt <ant.scott227@gmail.com>
2023-11-12 21:42:33 -0800

    week 5 done

--- interview-questions.md
+++ interview-questions.md
@@ -8,32 +8,32 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What does it mean to implement a TDD workflow?
 
-Your answer:
+Your answer: To implement a "TDD workflow" refers to a style of programming which test-driven development is the focus. This method consists of writing a small portion of the code that describes what the code should do, then another that runs the test and then makes the test successful. Then lastly repeats the process.
 
-Researched answer:
+Researched answer: Test-driven development is a popular development workflow that consists of three (3) parts: unit testing, programming, and refactoring. What this looks like is: initially writing a unit of test cases that before creating the actual code. The first set of code describes the action or intent of the code, and has a "positive" failure when tested. The next step is then writing the smallest amount of code that will make the test pass. Lastly any refractor code is added and the steps are repeated.
 
 2. What is a relational database? Are there other kinds of databases?
 
-Your answer:
+Your answer: A relational database is a database that is made up of tables that hold stored information. There are other types of databases aside from relational.
 
-Researched answer:
+Researched answer: A relational database stores data in tables that are comprised of rows and columns. Its basic functions are to read, create, update, and delete data. Other types of databases are hierarchical databases, non-relational databases, and object oriented databases.
 
-3. Why is an ORM?
+3. Why is an ORM? 
 
-Your answer:
+Your answer: *did not recall discussing ORM by name, and could not think of an example*
 
-Researched answer:
+Researched answer: ORM, or object relational mapping is the technique used to connect objects of an application to the tables of a relational database management system. It creates the connection or bridge between object-oriented programs and databases. This usually looks like using a command like 'SELECT * FROM users WHERE...' to retrieve information from another database system.
 
 4. STRETCH: What is the difference between inheritance in JavaScript and inheritance in Ruby?
 
-Researched answer:
+Researched answer: JavaScript's system is based on prototypes not classes. This means that when working with class inheritance in JavaScript, the class inheritance is made on top of the prototype inheritance. This connects the child or sub class to the parent or super class. In other words, many sub-classes can be created and inherit functions from the same parent or super class. Ruby on the other hand does not support multiple class inheritance, meaning each sub class will have its own parent
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-1. Model validations:
+1. Model validations: the process of confirming that the model achieves its intended purpose.
 
-2. Params:
+2. Params: variables that are defined in method definition and represent the ability of a method to accept arguments.
 
-3. API:
+3. API: "application program interface" - a program or interface used by developers that allows them to interact and communicate with software components or resources from other programs.
--- javascript-challenges.test.js
+++ javascript-challenges.test.js
@@ -1,5 +1,7 @@
 // ASSESSMENT 5: JavaScript Coding Practical Questions with Jest
 
+const { count } = require("console")
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -14,30 +16,167 @@
 
 // a) Create a test with expect statements using the variables provided.
 
-const secretCodeWord1 = "Lackadaisical"
-// Expected output: "L4ck4d41s1c4l"
-const secretCodeWord2 = "Gobbledygook"
-// Expected output: "G0bbl3dyg00k"
-const secretCodeWord3 = "Eccentric"
-// Expected output: "3cc3ntr1c"
+
+// describe("vowelToNumber", () => {
+//     it("takes in a string and returns a string with a coded message", () => {
+//         const secretCodeWord1 = "Lackadaisical"
+//         expect(vowelToNumber(secretCodeWord1)).toEqual("Lackadaisical")
+//         // Expected output: "L4ck4d41s1c4l"
+        
+//         const secretCodeWord2 = "Gobbledygook"
+//         expect(vowelToNumber(secretCodeWord2)).toEqual("Gobbledygook")
+//         // Expected output: "G0bbl3dyg00k"
+
+//         const secretCodeWord3 = "Eccentric"
+//         expect(vowelToNumber(secretCodeWord3)).toEqual("Eccentric")
+//         // Expected output: "3cc3ntr1c"
+//     })
+// })
+
+
+// OUTPUT: ReferenceError: vowelToNumber is not defined
+
 
 // b) Create the function that makes the test pass.
 
 // Pseudo code:
+
+//INPUT: a string
+//OUTPUT: a returned string that converts "a" to 4, "e" to 3, "i" to 1, and "o" to 0.
+
+// const vowelToNumber = .toLowerCase(string) => {
+//     let codeCreater = "";
+
+//     for (let i = 0; i < string.length; i++) {
+//         let charCode = string[i];
+
+//         switch (charCode) {
+//             case 'a':
+//                 codeCreater += '4';
+//                 break;
+//             case 'e':
+//                 codeCreater += '3';
+//                  break;
+//             case 'i':
+//                 codeCreater += '1';
+//                  break;
+//             case 'o':
+//                 codeCreater += '0';
+//                 break;
+//             default:
+//                 codeCreater += charCode;
+//         }
+//     }
+//     // .toLowerCase().split('')
+//     // .filter('a' = '4', 'e' = '3', 'i' = '1', 'o'= '0')
+//     // .join(' ')
+//     return codeCreater
+// }
+
+// const secretCodeWord1 = "Lackadaisical"
+// const secretCodeWord2 = "Gobbledygook"
+// const secretCodeWord3 = "Eccentric"
+
+// console.log(vowelToNumber(secretCodeWord1))
+// console.log(voweltoNumber(secretCodeWord2))
+// console.log(vowelToNumber(secretCodeWord3))
+
+// create function that holds substitute letter value and iterates through string
+//2nd option
+
+
 
 // --------------------2) Create a function that takes in an array of 5 numbers and determines whether or not the array is a "full house". A full house is exactly one pair and one three of a kind.
 
 // a) Create a test with expect statements using the variable provided.
 
-const hand1 = [5, 5, 5, 3, 3]
-// Expected output: true
-const hand2 = [5, 5, 3, 3, 4]
-// Expected output: false
-const hand3 = [5, 5, 5, 5, 4]
-// Expected output: false
-const hand4 = [7, 2, 7, 2, 7]
-// Expected output: true
+describe("pokerHand", () => {
+    test("should return true if hand has a pair", () => {
+        const hand1 = [5, 5, 5, 3, 3]
+        expect(pokerHand(hand1)).toEqual(true)
+        // Expected output: true
+
+        const hand2 = [5, 5, 3, 3, 4]
+        expect(pokerHand(hand2)).toEqual(false)
+         // Expected output: false
+
+        const hand3 = [5, 5, 5, 5, 4]
+        expect(pokerHand(hand3)).toEqual(false)
+         // Expected output: true
+
+        const hand4 = [7, 2, 7, 2, 7]
+        expect(pokerHand(hand4)).toEqual(true)
+        // Expected output: true
+    })
+
+    test("should return true if hand has three of a kind", () => {
+        const hand1 = [5, 5, 5, 3, 3]
+        expect(pokerHand(hand1)).toEqual(true)
+        // Expected output: true
+        
+        const hand2 = [5, 5, 3, 3, 4]
+        expect(pokerHand(hand2)).toEqual(false)
+        // Expected output: false
+        
+        const hand3 = [5, 5, 5, 5, 4]
+        expect(pokerHand(hand3)).toEqual(false)
+        // Expected output: false
+
+        const hand4 = [7, 2, 7, 2, 7]
+        expect(pokerHand(hand4)).toEqual(true)
+        // Expected output: true
+    })
+})
+// OUTPUT: ReferenceError: pokerHand is not defined
 
 // b) Create the function that makes the test pass.
 
 // Pseudo code:
+//INPUT: an array
+//OUTPUT: Boolean value true if array has a pair and three of a kind; false for everything else
+
+const pokerHand = (arr) => {
+    const cardCount = {}
+  
+    //create for loop to count each number in array and holds its value & iterates through to see if next number is the same. If it isn't, the count goes back to 0
+    for (let num of arr) {
+      cardCount[num] = (cardCount[num] || 0) + 1
+    }
+  
+    // create Boolean value for a Pair & Three of a Kind
+    let hasPair = false;
+    let hasThreeOfAKind = false;
+  
+    // conditional for loop to count values from "cardCount"
+    for (let count of Object.values(cardCount)) {
+      if (count === 2) {
+        hasPair = true;
+      } else if (count === 3) {
+        hasThreeOfAKind = true;
+      }
+    }
+  
+    // conditional for loop for "full house"
+    if (hasPair && hasThreeOfAKind) {
+      return true
+    } else {
+      return false
+    }
+  }
+  
+  const hand1 = [5, 5, 5, 3, 3]
+  const hand2 = [5, 5, 3, 3, 4]
+  const hand3 = [5, 5, 5, 5, 4]
+  const hand4 = [7, 2, 7, 2, 7]
+
+  console.log(pokerHand(hand1))
+  console.log(pokerHand(hand2))
+  console.log(pokerHand(hand3))
+  console.log(pokerHand(hand4))
+ 
+  
+  
+  
+  
+  
+  
